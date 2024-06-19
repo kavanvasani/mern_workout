c7b2973af817c7add2739c78b8e8bca14d730829
Kavan Vasani <kavan.vasani@gmail.com>
2024-06-18 19:30:37 -0700

    Added delete functionality

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import UseWorkoutsContext from '../hooks/UseWorkoutsContext'
 
 const WorkoutDetails = ({workout}) => {
+  const {dispatch} = UseWorkoutsContext()
+
+  const handleClick = async() => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if(response.ok){
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
   return (
     <div className='workout-details'>
         <h4>{workout.title}</h4>
         <p><strong>Weight (kg): </strong>{workout.weight}</p>
         <p><strong>Reps: </strong>{workout.reps}</p>
         <p>{workout.createdAt}</p>
+        <span onClick={handleClick}>Delete</span>
     </div>
   )
 }
